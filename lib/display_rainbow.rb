fbd8fbefbefb39bc513bc03e4e7caa65dedb172b
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-06-20 05:49:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
  def display_rainbow(rainbow)
-   rainbow=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+   rainbow=(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
    puts  "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}, G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[6]}, V: #{rainbow[6]}"
 end
