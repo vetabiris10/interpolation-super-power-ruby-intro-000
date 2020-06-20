cf37776d0459bef721af286c5d294aa5034172ac
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-06-20 05:57:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
  def display_rainbow(colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-   puts  "R: #{colors[0]}, O: #{colors[0]}, Y: #{colors[0]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[6]}, V: #{colors[6]}"
+   puts  "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
